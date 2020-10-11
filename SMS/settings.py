ab9f1017311d7cf9bc07b48168dbe6a6141e4bf7
surajit <surajit@poweredbypeople.io>
2020-10-11 15:56:15 +0300

    minor refactor

--- SMS/settings.py
+++ SMS/settings.py
@@ -12,15 +12,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's&f+)qq9))sdf^$vm!=(lbd+amf-x@f*l*ipb@qxa&esd2#^j3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.africastalking'
+    'africastalking',
+    'message'
 ]
 
 MIDDLEWARE = [
