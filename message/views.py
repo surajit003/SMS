19a8cbeef0260d53e519fbd947fab4b56844b9e1
surajit <surajit@poweredbypeople.io>
2020-10-15 02:05:53 +0300

    added view for bulk sms upload

--- message/views.py
+++ message/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from africastalking.utils import send_sms_via_at
+from .forms import FileUploadForm
 
 # Create your views here.
+
+
+def bulk_sms_upload(request):
+    if request.method == "POST":
+        form = FileUploadForm(request.POST, request.FILES)
+        return render(request, "message/file_upload.html", {"form": form})
