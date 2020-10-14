c784dcbf8ec0778fdf912bd91ee916171aa13612
surajit <surajit@poweredbypeople.io>
2020-10-15 02:02:00 +0300

    added fileupload modelform

---
+++ message/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import FileUpload
+
+
+class FileUploadForm(forms.ModelForm):
+    class Meta:
+        model = FileUpload
+        fields = (
+            "name",
+            "document",
+        )
